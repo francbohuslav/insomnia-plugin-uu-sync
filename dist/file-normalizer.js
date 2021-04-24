ba303f78da70202f13e4f4ddf4814d372943d20e
Bohuslav Franc <bohuslav.franc@unicornuniverse.eu>
2021-04-24 15:27:32 +0200

    ES6

--- dist/file-normalizer.js
+++ dist/file-normalizer.js
@@ -1,41 +1,39 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var FileNormalizer = /** @class */ (function () {
-    function FileNormalizer() {
-    }
-    FileNormalizer.prototype.normalizeExport = function (oneLineJson) {
-        var content = JSON.parse(oneLineJson);
+class FileNormalizer {
+    normalizeExport(oneLineJson) {
+        const content = JSON.parse(oneLineJson);
         content.__export_date = "2020-01-01T00:00:00.000Z";
         content.resources.forEach(this.setTimestamps);
         content.resources.sort(this.compareResources.bind(this));
         // Delete obsolete
-        content.resources.forEach(function (resource) {
+        content.resources.forEach((resource) => {
             if (resource.body && resource.body.__uuSyncText) {
                 delete resource.body.__uuSyncText;
             }
         });
         return content;
-    };
-    FileNormalizer.prototype.normalizeImport = function (content) {
+    }
+    normalizeImport(content) {
         this.getRequestsWithBody(content.resources).forEach(this.importMultiLineRequests);
         return content;
-    };
-    FileNormalizer.prototype.getRequestsWithBody = function (resources) {
-        return resources.filter(function (resource) { return resource._type == "request" && resource.body; });
-    };
-    FileNormalizer.prototype.setTimestamps = function (resource) {
+    }
+    getRequestsWithBody(resources) {
+        return resources.filter((resource) => resource._type == "request" && resource.body);
+    }
+    setTimestamps(resource) {
         if (resource.modified) {
             resource.modified = 1600000000000;
         }
         if (resource.created) {
             resource.created = 1600000000000;
         }
-    };
-    FileNormalizer.prototype.compareResources = function (a, b) {
+    }
+    compareResources(a, b) {
         return this.getSortKey(a).localeCompare(this.getSortKey(b));
-    };
-    FileNormalizer.prototype.getSortKey = function (resource) {
-        var key = "";
+    }
+    getSortKey(resource) {
+        let key = "";
         switch (resource._type) {
             case "workspace":
                 key = "0";
@@ -59,14 +57,13 @@ var FileNormalizer = /** @class */ (function () {
         }
         key += resource._id;
         return key;
-    };
-    FileNormalizer.prototype.importMultiLineRequests = function (resource) {
+    }
+    importMultiLineRequests(resource) {
         if (resource.body.__uuSyncText) {
             resource.body.text = resource.body.__uuSyncText.join("\n");
             delete resource.body.__uuSyncText;
         }
-    };
-    return FileNormalizer;
-}());
+    }
+}
 exports.default = FileNormalizer;
 //# sourceMappingURL=file-normalizer.js.map
--- dist/workspace-saver.js
+++ dist/workspace-saver.js
@@ -8,220 +8,104 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var __spreadArray = (this && this.__spreadArray) || function (to, from) {
-    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
-        to[j] = from[i];
-    return to;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var file_normalizer_1 = __importDefault(require("./file-normalizer"));
-var fs = require("fs");
-var path = require("path");
-var promisify = require("util").promisify;
-var mkdir = promisify(fs.mkdir);
-var writeFile = promisify(fs.writeFile);
-var readFile = promisify(fs.readFile);
-var readdir = promisify(fs.readdir);
-var unlink = promisify(fs.unlink);
-var WorkspaceSaver = /** @class */ (function () {
-    function WorkspaceSaver(insomniaFilePath) {
+const file_normalizer_1 = __importDefault(require("./file-normalizer"));
+const fs = require("fs");
+const path = require("path");
+const promisify = require("util").promisify;
+const mkdir = promisify(fs.mkdir);
+const writeFile = promisify(fs.writeFile);
+const readFile = promisify(fs.readFile);
+const readdir = promisify(fs.readdir);
+const unlink = promisify(fs.unlink);
+class WorkspaceSaver {
+    constructor(insomniaFilePath) {
         this.insomniaFilePath = insomniaFilePath;
         this.folderPath = insomniaFilePath + "-resources";
         this.workspaceFile = path.join(this.folderPath, "_workspace.json");
     }
-    WorkspaceSaver.prototype.exportOneFile = function (jsonObject) {
-        return __awaiter(this, void 0, void 0, function () {
-            var formattedJson;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        formattedJson = JSON.stringify(jsonObject, null, 2);
-                        return [4 /*yield*/, writeFile(this.insomniaFilePath, formattedJson)];
-                    case 1:
-                        _a.sent();
-                        return [2 /*return*/];
-                }
-            });
+    exportOneFile(jsonObject) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const formattedJson = JSON.stringify(jsonObject, null, 2);
+            yield writeFile(this.insomniaFilePath, formattedJson);
         });
-    };
-    WorkspaceSaver.prototype.exportMultipleFiles = function (jsonObject) {
-        return __awaiter(this, void 0, void 0, function () {
-            var resourcesOnDisk, filesToRemove, _i, _a, resource, _b, filesToRemove_1, fileName, fullFilePath, formattedJson;
-            return __generator(this, function (_c) {
-                switch (_c.label) {
-                    case 0:
-                        jsonObject = JSON.parse(JSON.stringify(jsonObject));
-                        return [4 /*yield*/, mkdir(this.folderPath, { recursive: true })];
-                    case 1:
-                        _c.sent();
-                        return [4 /*yield*/, this.getResourcesFiles()];
-                    case 2:
-                        resourcesOnDisk = _c.sent();
-                        filesToRemove = resourcesOnDisk.filter(function (fileName) { return jsonObject.resources.filter(function (r) { return r._id + ".json" == fileName; }).length == 0; });
-                        _i = 0, _a = jsonObject.resources;
-                        _c.label = 3;
-                    case 3:
-                        if (!(_i < _a.length)) return [3 /*break*/, 6];
-                        resource = _a[_i];
-                        return [4 /*yield*/, this.saveResource(resource)];
-                    case 4:
-                        _c.sent();
-                        _c.label = 5;
-                    case 5:
-                        _i++;
-                        return [3 /*break*/, 3];
-                    case 6:
-                        _b = 0, filesToRemove_1 = filesToRemove;
-                        _c.label = 7;
-                    case 7:
-                        if (!(_b < filesToRemove_1.length)) return [3 /*break*/, 10];
-                        fileName = filesToRemove_1[_b];
-                        fullFilePath = path.join(this.folderPath, fileName);
-                        return [4 /*yield*/, unlink(fullFilePath)];
-                    case 8:
-                        _c.sent();
-                        _c.label = 9;
-                    case 9:
-                        _b++;
-                        return [3 /*break*/, 7];
-                    case 10:
-                        jsonObject.resources = jsonObject.resources.map(this.getResourceIdWithName);
-                        formattedJson = JSON.stringify(jsonObject, null, 2);
-                        return [4 /*yield*/, writeFile(this.workspaceFile, formattedJson)];
-                    case 11:
-                        _c.sent();
-                        return [2 /*return*/];
-                }
-            });
+    }
+    exportMultipleFiles(jsonObject) {
+        return __awaiter(this, void 0, void 0, function* () {
+            jsonObject = JSON.parse(JSON.stringify(jsonObject));
+            yield mkdir(this.folderPath, { recursive: true });
+            const resourcesOnDisk = yield this.getResourcesFiles();
+            const filesToRemove = resourcesOnDisk.filter((fileName) => jsonObject.resources.filter((r) => r._id + ".json" == fileName).length == 0);
+            for (let resource of jsonObject.resources) {
+                yield this.saveResource(resource);
+            }
+            for (let fileName of filesToRemove) {
+                const fullFilePath = path.join(this.folderPath, fileName);
+                yield unlink(fullFilePath);
+            }
+            jsonObject.resources = jsonObject.resources.map(this.getResourceIdWithName);
+            const formattedJson = JSON.stringify(jsonObject, null, 2);
+            yield writeFile(this.workspaceFile, formattedJson);
         });
-    };
-    WorkspaceSaver.prototype.importMultipleFiles = function () {
-        return __awaiter(this, void 0, void 0, function () {
-            var formattedJson, jsonObject, normalizer, formattedJson, jsonObject, resourcesIds, _i, resourcesIds_1, resourceIdWithName, resourceId, _a, _b;
-            return __generator(this, function (_c) {
-                switch (_c.label) {
-                    case 0:
-                        if (!!fs.existsSync(this.workspaceFile)) return [3 /*break*/, 2];
-                        return [4 /*yield*/, readFile(this.insomniaFilePath, "utf8")];
-                    case 1:
-                        formattedJson = _c.sent();
-                        jsonObject = JSON.parse(formattedJson);
-                        normalizer = new file_normalizer_1.default();
-                        jsonObject = normalizer.normalizeImport(jsonObject);
-                        return [2 /*return*/, jsonObject];
-                    case 2: return [4 /*yield*/, readFile(this.workspaceFile, "utf8")];
-                    case 3:
-                        formattedJson = _c.sent();
-                        jsonObject = JSON.parse(formattedJson);
-                        resourcesIds = __spreadArray([], jsonObject.resources);
-                        jsonObject.resources = [];
-                        _i = 0, resourcesIds_1 = resourcesIds;
-                        _c.label = 4;
-                    case 4:
-                        if (!(_i < resourcesIds_1.length)) return [3 /*break*/, 7];
-                        resourceIdWithName = resourcesIds_1[_i];
-                        resourceId = this.getResourceIdFromIdWithName(resourceIdWithName);
-                        _b = (_a = jsonObject.resources).push;
-                        return [4 /*yield*/, this.loadResource(resourceId)];
-                    case 5:
-                        _b.apply(_a, [_c.sent()]);
-                        _c.label = 6;
-                    case 6:
-                        _i++;
-                        return [3 /*break*/, 4];
-                    case 7: return [2 /*return*/, jsonObject];
+    }
+    importMultipleFiles() {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!fs.existsSync(this.workspaceFile)) {
+                const formattedJson = yield readFile(this.insomniaFilePath, "utf8");
+                let jsonObject = JSON.parse(formattedJson);
+                const normalizer = new file_normalizer_1.default();
+                jsonObject = normalizer.normalizeImport(jsonObject);
+                return jsonObject;
+            }
+            else {
+                const formattedJson = yield readFile(this.workspaceFile, "utf8");
+                const jsonObject = JSON.parse(formattedJson);
+                const resourcesIds = [...jsonObject.resources];
+                jsonObject.resources = [];
+                for (const resourceIdWithName of resourcesIds) {
+                    const resourceId = this.getResourceIdFromIdWithName(resourceIdWithName);
+                    jsonObject.resources.push(yield this.loadResource(resourceId));
                 }
-            });
+                return jsonObject;
+            }
         });
-    };
-    WorkspaceSaver.prototype.getResourcesFiles = function () {
-        return __awaiter(this, void 0, void 0, function () {
-            var files;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0: return [4 /*yield*/, readdir(this.folderPath)];
-                    case 1:
-                        files = _a.sent();
-                        return [2 /*return*/, files.filter(function (file) { return file != "_workspace.json"; })];
-                }
-            });
+    }
+    getResourcesFiles() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const files = yield readdir(this.folderPath);
+            return files.filter((file) => file != "_workspace.json");
         });
-    };
-    WorkspaceSaver.prototype.getResourceIdWithName = function (resource) {
+    }
+    getResourceIdWithName(resource) {
         return resource._id + " | " + resource.name;
-    };
-    WorkspaceSaver.prototype.getResourceIdFromIdWithName = function (resourceIdWithName) {
+    }
+    getResourceIdFromIdWithName(resourceIdWithName) {
         return resourceIdWithName.split("|")[0].trim();
-    };
-    WorkspaceSaver.prototype.saveResource = function (resource) {
-        return __awaiter(this, void 0, void 0, function () {
-            var fullFilePath, formattedJson;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        fullFilePath = path.join(this.folderPath, resource._id + ".json");
-                        if (resource._type == "request" && resource.body && resource.body.text) {
-                            resource.body.text = resource.body.text.split("\n");
-                        }
-                        formattedJson = JSON.stringify(resource, null, 2);
-                        return [4 /*yield*/, writeFile(fullFilePath, formattedJson)];
-                    case 1:
-                        _a.sent();
-                        return [2 /*return*/];
-                }
-            });
+    }
+    saveResource(resource) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const fullFilePath = path.join(this.folderPath, resource._id + ".json");
+            if (resource._type == "request" && resource.body && resource.body.text) {
+                resource.body.text = resource.body.text.split("\n");
+            }
+            const formattedJson = JSON.stringify(resource, null, 2);
+            yield writeFile(fullFilePath, formattedJson);
         });
-    };
-    WorkspaceSaver.prototype.loadResource = function (resourceId) {
-        return __awaiter(this, void 0, void 0, function () {
-            var fullFilePath, formattedJson, resource;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        fullFilePath = path.join(this.folderPath, resourceId + ".json");
-                        return [4 /*yield*/, readFile(fullFilePath)];
-                    case 1:
-                        formattedJson = _a.sent();
-                        resource = JSON.parse(formattedJson);
-                        if (resource._type == "request" && resource.body && resource.body.text) {
-                            resource.body.text = resource.body.text.join("\n");
-                        }
-                        return [2 /*return*/, resource];
-                }
-            });
+    }
+    loadResource(resourceId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const fullFilePath = path.join(this.folderPath, resourceId + ".json");
+            const formattedJson = yield readFile(fullFilePath);
+            const resource = JSON.parse(formattedJson);
+            if (resource._type == "request" && resource.body && resource.body.text) {
+                resource.body.text = resource.body.text.join("\n");
+            }
+            return resource;
         });
-    };
-    return WorkspaceSaver;
-}());
+    }
+}
 exports.default = WorkspaceSaver;
 //# sourceMappingURL=workspace-saver.js.map
